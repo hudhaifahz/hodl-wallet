c3db23b4d37ae689038be1d1b548b280e28d6369
hudhaifahz <hudhaifahz@gmail.com>
2018-01-07 22:02:10 -0800

    chart.js test run

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, View, ScrollView, StyleSheet, Text } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
+import Chart from 'chart.js';
 
 import FetchCoinData from '../Actions/FetchCoinData';
 import ResetTotal from '../Actions/ResetTotal';
@@ -20,6 +21,56 @@ class CryptoContainer extends Component {
   refreshData() {
     this.props.ResetTotal();
     this.props.FetchCoinData();
+  }
+
+  renderPieChart(){
+    const { slice } = this.props;
+    if (!slice.hasError) {
+      <canvas id="myChart" width="400" height="400"></canvas>
+      <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [{
+                  label: '# of Votes',
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true
+                      }
+                  }]
+              }
+          }
+      });
+      </script>
+    } else {
+      return (
+        <Text>{slice.errorMessage}</Text>
+      )
+    }
   }
 
   renderCoinCards() {
